60eb7828c5ac35bb9a223ebadc8936a63ed6756c
valurp <vpv1@hi.is>
2020-02-19 15:21:30 +0000

    if expression

--- NanoMorphoParser.java
+++ NanoMorphoParser.java
@@ -11,7 +11,47 @@ public class NanoMorphoParser {
 
 	private static void function() {
 		// Add code here
-		
+
+	}
+
+	private static void ifexpr() {
+		if (nml.getToken1() != 1001) syntaxError("if", nml.getLexeme());
+		nml.advance();
+		if (nml.getToken1() != 40) syntaxError("(", nml.getLexeme());
+		nml.advance();
+		expr();
+		if (nml.getToken1() != 41) syntaxError(")", nml.getLexeme());
+		nml.advance();
+		if (nml.getToken1() != 123) syntaxError("{", nml.getLexeme());
+		expr();
+		nml.advance();
+		if (nml.getToken1() != 125) syntaxError("}", nml.getLexeme());
+
+
+		//elsif token
+		while (nml.getToken1() == 1005) {
+			nml.advance(); //til þess að komast út úr elsif tokeninu
+			if (nml.getToken1() != 40) syntaxError("(", nml.getLexeme());
+			nml.advance();
+			expr();
+			if (nml.getToken1() != 41) syntaxError(")", nml.getLexeme());
+			nml.advance();
+			if (nml.getToken1() != 123) syntaxError("{", nml.getLexeme());
+			expr();
+			nml.advance();
+			if (nml.getToken1() != 125) syntaxError("}", nml.getLexeme());
+		}
+
+		//else token
+		if (nml.getToken1() == 1006) {
+			if (nml.getToken1() != 123) syntaxError("{", nml.getLexeme());
+			nml.advance();
+			expr();
+			nml.advance();
+			if (nml.getToken1() != 125) syntaxError("}", nml.getLexeme());
+		}
+
+		}
 	}
 
 }
