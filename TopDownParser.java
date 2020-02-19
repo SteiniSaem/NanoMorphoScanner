25e9deff2897d7a19465244d8eea28a75714bc15
kra33 <kra33@hi.is>
2020-02-19 13:06:40 +0000

    Remove lexer

--- TopDownParser.java
+++ TopDownParser.java
@@ -5,21 +5,3 @@ public class TopDownParser
 
 	}
 }
-
-private class Lexer
-{
-	private NanoMorphoLexer myLexer;
-	private int tok1, tok;
-	private String lex1, lex2;
-	public Lexer(String fileName)
-	{
-		this.myLexer = new NanoMorphoLexer(new FileReader(fileName));
-	}
-
-	public static void init()
-	{
-		tok1 = myLexer.yylex();
-		tok2 = myLexer.yylex();
-	}
-
-}
